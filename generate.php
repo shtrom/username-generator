5fe594e1a2f5483681025107b067898537da2c74
Olivier Mehani <shtrom@ssji.net>
2025-06-20 13:13:41 +1000

    Use filter_input to avoid XSS injection

--- generate.php
+++ generate.php
@@ -1,4 +1,9 @@
 <?php
+
+function get_var(string $var): ?string {
+    return filter_input(INPUT_GET, $var, FILTER_SANITIZE_STRING);
+}
+
 $file = "adjectives.txt";
 
 $lines1 = count(file($file));
@@ -9,7 +14,7 @@ $file = "nouns.txt";
 $lines1 = count(file($file));
 $lines = file( $file );
 
-if ($_GET['importantnouns']) {
+if (get_var("importantnouns")) {
     if (rand(1,3) == '1') {
         $it2 = $_GET['importantnouns'];
     }
@@ -25,13 +30,13 @@ $lines1 = count(file($file));
 $lines = file($file);
 $it3 = $lines[rand(0,$lines1)];
 
-if ($_GET['gm'] == 2) {
+if (get_var('gm') == '2') {
     $stuff = $it . $it2;
 } else {
     $stuff = $it . $it2 . $it3;
 }
 
-$final = $_GET['prefix'] . preg_replace('/\s+/','',$stuff) . $_GET['suffix'];
+$final = get_var('prefix') . preg_replace('/\s+/','',$stuff) . get_var('suffix');
 if (!$final) {
     header( 'Location: ?mk=1' ) ;
 }
